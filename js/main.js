33c2bbfa0eed0cece7478e3fcfa79f2876187a09
saramarin23 <saramarin.econ@gmail.com>
2019-11-11 00:47:48 +0100

    finish javascript functionalities

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 "use strict";
 
 const menuButton = document.querySelector(".nav-trigger");
+const navMenu = document.querySelector(".nav");
 const closeButton = document.querySelector(".nav-close");
-
-const closeImage = "../images/ico-close.svg";
-const menuImage = "../images/ico-menu.svg";
+const navTrigger = document.querySelector(".nav-trigger");
 
 function makeSthMenuBtn() {
   console.log("Has pinchado para desplegar el menú");
+  navMenu.classList.remove("js-hide");
+  menuButton.classList.add("js-hide");
+  closeButton.classList.remove("js-hide");
 }
 
 function makeSthCloseBtn() {
   console.log("Has clickado el botón de cerrar");
+  navMenu.classList.add("js-hide");
+  closeButton.classList.remove("js-hide");
+  navTrigger.classList.remove("js-hide");
 }
 
 menuButton.addEventListener("click", makeSthMenuBtn);
